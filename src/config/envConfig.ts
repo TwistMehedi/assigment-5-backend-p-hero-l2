10d75417614a9f95e27eab6b2b7cbeea279e6bc3
TwistMehedi <ataul1708@gmail.com>
2026-04-13 07:20:30 +0600

    remove better auth url

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -9,7 +9,6 @@ interface IEnvConfig {
   DATABASE_URL: string;
   PORT: string;
   BETTER_AUTH_SECRET: string;
-  BETTER_AUTH_URL: string;
   NODE_ENV: string;
   USER_EMAIL: string;
   USER_PASS: string;
@@ -29,7 +28,6 @@ const validateEnv = [
   "DATABASE_URL",
   "PORT",
   "BETTER_AUTH_SECRET",
-  "BETTER_AUTH_URL",
   "NODE_ENV",
   "USER_EMAIL",
   "USER_PASS",
@@ -55,7 +53,6 @@ const envConfig = (): IEnvConfig => ({
   DATABASE_URL: process.env.DATABASE_URL as string,
   PORT: process.env.PORT as string,
   BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
-  BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
   NODE_ENV: process.env.NODE_ENV as envarnMent,
   USER_EMAIL: process.env.USER_EMAIL as string,
   USER_PASS: process.env.USER_PASS as string,
